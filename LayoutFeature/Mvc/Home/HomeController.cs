b27202f482f4844e589fbd23e01958a1d8d18f13
Ponant <karim.murr@outlook.com>
2017-06-26 20:49:36 +0200

    Add files via upload

---
+++ LayoutFeature/Mvc/Home/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LayoutFeature.Mvc.Home
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("myroute")]
+        public IActionResult Index() => View();
+    }
+}
